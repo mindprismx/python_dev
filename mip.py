5fc6c34652ef008a4669d8ee60d914ab36f1d444
mindprismx <github@augros.org>
2025-07-17 22:26:20 -0500

    mandel-bros before mandel-hos

--- mip.py
+++ mip.py
@@ -6,9 +6,6 @@ import re
 url = "https://www.augros.org/wimipaqm.shtml"
 response = requests.get(url)
 
-# print(response.status_code)  # 200 = OK
-# print(response.text)  # HTML content
-
 match = re.search(
     r"ADDR\s+(\d+\.\d+\.\d+\.\d+)<br>NAME\s+([a-zA-Z0-9_.-]+)\.<font color=blue><b>([^<]+)</b>",
     response.text,
