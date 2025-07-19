db5ab1c8ea3f4866355f51ab0e41307b7de58baa
mindprismx <github@augros.org>
2025-07-18 20:29:06 -0500

    fixed re

--- mip.py
+++ mip.py
@@ -7,8 +7,12 @@ url = "https://www.augros.org/wimipaqm.shtml"
 response = requests.get(url)
 
 match = re.search(
-    r"ADDR\s+(\d+\.\d+\.\d+\.\d+)<br>NAME\s+([a-zA-Z0-9_.-]+)\.<font color=blue><b>([^<]+)</b>",
+    r"(?:ADDR\s+(\d+\.\d+\.\d+\.\d+)<br>)?NAME\s*([a-zA-Z0-9_.-]*)\.?(?:<font.*?><b>([^<]+)</b>)?",
     response.text,
 )
+
 if match:
-    print(match.group(1), f"{match.group(2)}.{match.group(3)}")
+    ip = match.group(1)
+    name = match.group(2)
+    domain = match.group(3)
+    print(ip, f"\t{domain}")
